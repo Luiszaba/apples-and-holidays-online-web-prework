e040b945a9ba6489c874e931cc25fcc8f3e1f835
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 21:53:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,8 +20,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   
   holiday_hash[:summer][:fourth_of_july][1]
-  
-  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -30,7 +28,6 @@ holiday_hash[:winter].each do |holiday, supplies|
 end
 end
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
 holiday_hash[:spring].each do |holiday, arg|
   arg << supply
@@ -38,20 +35,16 @@ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-
   holiday_hash[season][holiday_name]= supply_array
   holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-
 winter_supplies = holiday_hash[:winter].map do |holiday, supplies|
   supplies
 end
   winter_supplies.flatten
 end
-
-
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
