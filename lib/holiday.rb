45f84ad8847ee2a3f2d3df5e4cb8a1642187d873
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 21:50:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holidays.each do |holiday, supplies|
     puts "#{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
-
+end
   end
 end
 
