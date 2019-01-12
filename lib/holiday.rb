d0504ce5d5e9dc8f26ea62705bd8e172d347ba3d
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 21:38:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season, items|
-    
+  holiday_hash[:holiday_supplies].map do |array|
+    array.to_s
     
 
 end
