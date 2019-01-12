f2a75266ece3f3cd042bdbeffd7fdde989ff614d
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 21:31:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 winter_supplies = holiday_hash[:winter].map do |holiday, supplies|
+  winter_supplies
   winter_supplies.flatten
 end
 end
